a9e5d02663bee6307035ee8c2c118e9d18168105
Taiyeaba <tayebashams@gmail.com>
2025-03-01 22:53:34 +0600

    seven

--- javaScri.js
+++ javaScri.js
@@ -1,3 +1,30 @@
-document.getElementById('discover').addEventListener('click', function() {
+document.getElementById("discover").addEventListener('click', function() {
   window.location.href = "index2.html";
 })
+//6 ti button
+document.getElementById("completeBtn1").addEventListener('click', function() {
+  alert("Board updated successfully");
+  document.getElementById('completeBtn1').disabled = true;
+
+
+});
+
+
+document.getElementById('completeBtn2').addEventListener('click',function(){
+  alert("Board updated successfully");
+})
+
+document.getElementById('completeBtn3').addEventListener('click',function(){
+  alert("Board updated successfully");
+})
+document.getElementById('completeBtn4').addEventListener('click',function(){
+  alert("Board updated successfully");
+})
+document.getElementById('completeBtn5').addEventListener('click',function(){
+  alert("Board updated successfully");
+})
+document.getElementById('completeBtn6').addEventListener('click',function(){
+  alert("Board updated successfully");
+})
+
+
